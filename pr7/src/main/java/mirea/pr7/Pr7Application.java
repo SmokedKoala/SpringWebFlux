5166200a5914db237417d7ae39a6cbf795b3bb77
SmokedKoala <k.azarov@inbox.ru>
2022-11-30 23:12:16 +0300

    init

---
+++ pr7/src/main/java/mirea/pr7/Pr7Application.java
@@ -0,0 +1,13 @@
+package mirea.pr7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr7Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Pr7Application.class, args);
+  }
+
+}
